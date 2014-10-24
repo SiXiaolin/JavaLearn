b8c605ebd17f5bf7fe50fffc102c19c4d729987d
Si Xiaolin <sixiaolin067@gmail.com>
2014-10-24 17:33:21 +0800

    Java Collections
    example in Java Collections

---
+++ JukeBox.java
@@ -0,0 +1,38 @@
+package headfirst;
+import java.util.*;
+import java.io.*;
+
+
+public class JukeBox {
+	ArrayList<Song> songlist= new ArrayList<Song>();
+	
+	public static void main(String args[]){
+		new JukeBox().go();
+	}
+	
+	public void go(){
+		getSongs();
+		System.out.println(songlist);
+		Collections.sort(songlist);
+		System.out.println(songlist);
+	}
+	
+	void getSongs(){
+		try{
+			File file= new File("SongList.txt");
+			BufferedReader reader= new BufferedReader(new FileReader(file));
+			String line= null;
+			while((line=reader.readLine())!=null){
+				addSong(line);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	void addSong(String linetoParse){
+		String[] tokens= linetoParse.split("/");
+		songlist.add(new Song(tokens[0],tokens[1],tokens[2],tokens[3]));
+	}
+
+}
